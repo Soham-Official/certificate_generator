ef8bed3071227f0dda8bb3182277f7ab1728e4f6
Soham Chakraborty <soham.chakrabortyofficial@gmail.com>
2021-11-30 23:25:12 +0530

    new updates

--- certificate_generator/src/Components/Main.jsx
+++ certificate_generator/src/Components/Main.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
-import download from "downloadjs";
 import undraw from "../undraw.svg";
+import ScaleLoader from "react-spinners/ScaleLoader";
 import logo from "../logo.png";
 const Main = () => {
   const [name, setName] = useState("");
   const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const Validate = (e) => {
+    if (name === "") return;
+    setLoading(true);
     e.preventDefault();
+    setError("");
     fetch("http://localhost:5000/get_certificate", {
       method: "post",
       headers: {
@@ -20,44 +23,91 @@ const Main = () => {
     })
       .then((res) => res.json())
       .then((res) => {
-        if (res.error) return setError(res.error);
+        setName("");
+        if (res.err) return setError(res.err);
         setTimeout(() => {
           window.location.href = `http://localhost:5000/download/${res.token}`;
-        }, 100);
+          setLoading(false);
+        }, 1000);
       })
       .catch((err) => console.log(err));
   };
   return (
     <>
-    <div className="header">
-    <div className="logo"> <img src={logo} alt="" /> 
-    </div>
-    <div className="heading">
-    CODECHEF NSEC CHAPTER
-    </div>
-    </div>
-    <div className="pic">
-      <img src={undraw} alt="" />
-    </div>
-    <div className="login">
-	<h4>Provide the following details to get your certificate</h4>
-    <form>
-    	<input type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)} placeholder="codechef id" required="required" />
-        <input type="text" name="p" placeholder="Contest Code" required="required" />
-        {name ? (
-        <button className="btn" type="submit" onClick={Validate}><span>Get my Certificate</span></button>
-      ) : (
-        <button className="btn" disabled><span>Get my Certificate</span></button>
-      )}
-        </form>
-       
-</div>
-      
-	
+      <div className="header">
+        <div className="logo">
+          {" "}
+          <img src={logo} alt="" />
+        </div>
+        <div className="heading">CODECHEF NSEC CHAPTER</div>
+      </div>
+      <div
+        className="preloader"
+        style={{
+          position: "absolute",
+          display: "flex",
+          justifyContent: "center",
+          textAlign: "center",
+          width: "100%",
+          alignItems: "center",
+          height: "50vh",
+        }}
+      >
+        <ScaleLoader
+          color={"#4dd637"}
+          height={65}
+          width={12}
+          radius={5}
+          margin={4}
+          loading={loading}
+          size={250}
+        />
+      </div>
+      <div className="pic">
+        <img src={undraw} alt="" />
+      </div>
+      <div className="login">
+        {error ? (
+          <div
+            style={{
+              backgroundColor: "#e64543",
+              padding: "12px",
+              color: "white",
+            }}
+            role=""
+          >
+            <b>{error}</b>
+          </div>
+        ) : (
+          ""
+        )}
 
-      
+        <h4>Provide the following details to get your certificate</h4>
+        <form>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Codechef Username"
+            required="required"
+          />
+          <input type="text" name="p" placeholder="Contest Code" />
+          {name ? (
+            <button
+              className="btn"
+              type="submit"
+              onClick={Validate}
+              style={{ marginTop: "20px" }}
+            >
+              <span>Get my Certificate</span>
+            </button>
+          ) : (
+            <button className="btn" disabled style={{ marginTop: "20px" }}>
+              <span>Get my Certificate</span>
+            </button>
+          )}
+        </form>
+      </div>
     </>
   );
 };
--- index.js
+++ index.js
@@ -39,16 +39,21 @@ app.post("/get_certificate", (req, res) => {
         Username: name,
       })
       .then(function (record) {
+        if (name === undefined)
+          return res.json({
+            err: "You are not eligible for this certificate . If you think there is a mistake, contact the admins.",
+          });
         name = record.FullName;
-        
-        name=name.trim();
-        name=name.toLowerCase();
-        name= name.split(" ");
-
-        for (let i = 0; i < name.length; i++) {
-          name[i] = name[i][0].toUpperCase() + name[i].substr(1);
+        name = name.trim();
+        name = name.toLowerCase();
+        var splitStr = name.split(" ");
+        for (var i = 0; i < splitStr.length; i++) {
+          splitStr[i] =
+            splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
         }
-        name.join(" ");
+
+        name = splitStr.join(" ");
+
         console.log(name);
         const doc = new PDFDocument({
           layout: "landscape",
@@ -57,16 +62,15 @@ app.post("/get_certificate", (req, res) => {
         doc.pipe(fs.createWriteStream(`${name}.pdf`));
         doc.image("certificate_november.jpeg", 0, 0, { width: 850 });
         doc.font("fonts/RockoUltraFLF.ttf");
-        if(name.length>14){
+        if (name.length > 14) {
           doc.fontSize(30).fillColor("white").text(name, 300, 220, {
             align: "center",
           });
+        } else {
+          doc.fontSize(50).fillColor("white").text(name, 300, 220, {
+            align: "center",
+          });
         }
-        else{
-        doc.fontSize(50).fillColor("white").text(name, 300, 220, {
-          align: "center",
-        });
-      }
         doc.end();
         return res.json({
           name,
